c76c5a387403c37b08b02fa48610b8fa5374c84e
Alexandru Parosu <alexandru.parosu@econsor.de>
2025-10-21 10:27:22 +0200

    Test Response

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+document.getElementById("submit").onclick = getRestAPI();
+
+async function getRestAPI() {
+	var url = document.getElementById("url").value;
+	const response = await fetch(url);
+	const json = awai
+	response.json();
+	console.log(json);
+}
